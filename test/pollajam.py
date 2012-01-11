c927ec5aa64b37293a842458f18011c92dfc06aa
Shane R. Spencer <shane@bogomip.com>
2012-01-10 17:30:55 -0900

    Rewriting client code completely

---
+++ test/pollajam.py
@@ -0,0 +1,17 @@
+import socket
+import logging
+
+import tornado.ioloop
+
+import amity.client
+
+if __name__ == "__main__":
+    import sys
+
+    logging.basicConfig(stream=sys.stdout, level=logging.DEBUG,
+        format='%(asctime)s %(process)d %(filename)s %(lineno)d %(levelname)s #| %(message)s',
+        datefmt='%H:%M:%S')
+
+    testingserver = amity.client.AJAMClient(host='testserver1', proxy_host='10.10.0.1', proxy_port=3128)
+
+    tornado.ioloop.IOLoop.instance().start()
